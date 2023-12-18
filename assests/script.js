e4c7c34ba519acc7e35ff098625ab0c84620041d
Rafael Ehlke <heeyrafaa@gmail.com>
2023-12-18 13:20:31 -0300

    add ./ setattributes

--- assests/script.js
+++ assests/script.js
@@ -8,11 +8,11 @@ function toggleMode() {
   // substituir a Imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "/assests/assets/avatar-light.png")
+    img.setAttribute("src", "./assests/assets/avatar-light.png")
     img.setAttribute("alt", "foto de rafael, com braços cruzados, encostado em uma saveiro preta")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "/assests/assets/avatar.png")
+    img.setAttribute("src", "./assests/assets/avatar.png")
     img.setAttribute("alt", "foto de rafael, sinalizando beleza, em pé em uma escada")
   }
 }
